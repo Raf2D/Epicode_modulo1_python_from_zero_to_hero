2851700e9773a37165b80d0ec7c1ec9755f3cf79
Raffaele Caputi <caputiraffaele@gmail.com>
2026-04-07 21:41:29 +0200

    aggiunta file con esercizi tipi di dati
    aggiunta file contenente i 4 esercizi che vengono descritti nel video tipi di dati del corso python di epicode del modulo 1.

---
+++ esercizi_e_sorgenti_tipi_di_dati/esercizi_tipi_di_dati.py
@@ -0,0 +1,37 @@
+#Esercizio 1: Crea una variabile x con valore 7 e una y con valore 3. Stampa la somma dei due numeri
+
+print("------ESERCIZIO 1.-------------------|")
+x = 7
+y = 3 
+print("Questa è la somma tra x e y: ",x+y)
+
+print("\n")
+#Esercizio 2: Crea una variabile prezzo con valore 19.99 e una quantità con valore 3. Stampa il totale (prezzo x quantità)
+
+print("------ESERCIZIO 2.-------------------|")
+prezzo = 19.99
+quantita = 3
+print("Ecco il totale tra prezzo e quantita: ",prezzo*quantita)
+
+print("\n")
+#Esercizio 3: Crea una variabile numero con valore 10. Stampa True se il numero è maggiore di 5, altrimenti False
+
+print("------ESERCIZIO 3.-------------------|")
+numero = 10
+flag = False
+
+if(numero > 5):
+    flag = True
+    print(flag)
+else:
+    print(flag)
+
+print("\n")
+#Esercizio 4: Prova a combinare un int e un float: assegna a a = 5 e b = 2.5, poi calcola la loro somma
+print("------ESERCIZIO 4.-------------------|")
+
+a = 5
+b = 2.5
+print("Ecco la somma tra a e b: ", a+b)
+
+print("\n")
