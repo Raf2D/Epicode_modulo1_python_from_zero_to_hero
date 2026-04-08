a9e1178dccdaa33a6d72633fd39010d5817f1157
Raffaele Caputi <caputiraffaele@gmail.com>
2026-04-08 10:30:11 +0200

    esercizi casting
    Svolto esercizi casting descritti nella lezione sul Casting del modulo 1 : python from zero to hero.

---
+++ 2_esercizi_e_sorgenti_casting/esercizi_casting.py
@@ -0,0 +1,11 @@
+#Esercizio 1: chieda all'utente di inserire un numero intero
+
+a = int(input("Inserisci un numero intero: "))
+
+#Esercizio 2: converta questo numero in numero decimale (float) e lo stampi
+
+print("Ecco il numero intero inserito trasformato in tipo float: ", float(a))
+
+#Esercizio 3: converta lo stesso numero in stringa e lo stampi insieme a un messaggio
+
+print("Ecco il numero float trasfrormato in string: " + str(a))
