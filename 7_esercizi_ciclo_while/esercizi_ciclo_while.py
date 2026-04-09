c35a0e227d0364c73762194795cf3d93e839d655
Raffaele Caputi <caputiraffaele@gmail.com>
2026-04-09 12:36:32 +0200

    esercizio con ciclo while
    file contenente l'esercizio sul ciclo while menzionato alla fine della lezione appunto del ciclo while del corso Epicode

---
+++ 7_esercizi_ciclo_while/esercizi_ciclo_while.py
@@ -0,0 +1,15 @@
+#Esercizio lezione "Ciclo While"
+
+#Prova a scrivere un programma utilizzando il ciclo while che:
+#chiede all'utente di inserire un numero positivo.
+#continua a chiedere finchè l'utente non inserisce un numero positivo (>0)
+#quando il numero è positivo, stampa "Hai inserito il numero positivo: X" e termina
+
+numero = 0
+
+while numero <= 0 :
+
+    numero = float(input("Inserisci un numero positivo: "))
+
+    if numero > 0 : 
+        print("Hai inserito il numero positivo maggiore di 0: ", numero)
