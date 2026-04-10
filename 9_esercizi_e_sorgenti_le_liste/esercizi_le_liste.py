cdd21e327435f5c5feb224d9b8e45af2d4e38416
Raffaele Caputi <caputiraffaele@gmail.com>
2026-04-10 16:44:42 +0200

    creato file con gli esercizi della lezione Le liste
    aggiungo la cartella con il file contenente gli esercizi menzionati a fine lezione "Le liste" del modulo 1 del corso di epicode.

---
+++ 9_esercizi_e_sorgenti_le_liste/esercizi_le_liste.py
@@ -0,0 +1,13 @@
+#Esercizio lezione "Le liste":
+#crea una lista con 5 numeri
+#aggiungi il terzo numero
+#stampa la lista finale
+
+
+lista = [1,2,3,4,5]
+
+lista.append(20)
+
+lista[2] = 88
+
+print("Lista finale: ", lista)
